77b9f1780c3f6938acf2ab099525b88b35e5127c
Sachin Maurya <100995198+SachinMaurya01@users.noreply.github.com>
2023-02-18 11:23:37 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatgpt-sqc2.onrender.com', {
+    const response = await fetch('https://chatgpt-yisf.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
