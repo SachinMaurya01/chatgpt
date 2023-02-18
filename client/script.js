555cfd4d81421aa1533dd1f68a966ebd28b27655
Sachin Maurya <100995198+SachinMaurya01@users.noreply.github.com>
2023-02-18 10:27:06 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e)=>{
 
     //fetch data from server -> bot's response
 
-    const response = await fetch('https://chatgpt-7s5v.onrender.com',{
+    const response = await fetch('https://chatgpt-sqc2.onrender.com',{
         method:'POST',
         headers:{
             'Content-Type':'application/json'
